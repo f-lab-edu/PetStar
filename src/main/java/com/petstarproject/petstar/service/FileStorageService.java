756e67eb61f60e8b23245cb4b0518c91b065c95a
gwangyeol-park <samjjng01@gmail.com>
2026-01-19 00:55:30 +0900

    Revert "Revert "Revert "fix: PostingService 삭제 로직 개선"""
    This reverts commit 98b1d5abdf1d4a04f5b9389d4dbcaa17dec21ec6.

--- src/main/java/com/petstarproject/petstar/service/FileStorageService.java
+++ src/main/java/com/petstarproject/petstar/service/FileStorageService.java
@@ -2,12 +2,8 @@ package com.petstarproject.petstar.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 public interface FileStorageService {
     String upload(MultipartFile file, String key);
 
     void delete(String key);
-
-    void deleteAll(List<String> keys);
 }
